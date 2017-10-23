35bb68dc9f83d19c3a94eac0e191d0f07b651e99
mjrb <www.w.com123@gmail.com>
2017-10-22 23:36:53 -0400

    fix schema on default message

--- dbinit.rb
+++ dbinit.rb
@@ -2,4 +2,4 @@ load "loadConfig.rb"
 load "mdb.rb"
 $client[:index].insert_one({:counter => "visits", :value => 0})
 $client[:users].insert_one({:name => "admin", :password => "password"})
-$client[:messages].insert_one({:title => "default message", :body =>"this is a default message", :email =>"user@example.com"})
+$client[:messages].insert_one({:title => "default message", :message =>"this is a default message", :email =>"user@example.com"})
