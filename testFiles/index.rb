a31621a2cffeaacd7c53b1445a1b9dea90b35da3
mjrb <www.w.com123@gmail.com>
2017-10-22 22:54:34 -0400

    refactoring

--- testFiles/index.rb
+++ testFiles/index.rb
@@ -6,12 +6,12 @@ def greeting
   bothFilled=!(($get[:name]=="") or ($get[:surname]==""))
 
   if hasBoth and bothFilled
-    tmp="you are the number "+$views.to_s+" visitor, "+$get[:name]+" "+$get[:surname]+"!"
+    tmp="you are the number "+$visits.to_s+" visitor, "+$get[:name]+" "+$get[:surname]+"!"
   elsif $get==Hash.new
-    tmp="you are the number "+$views.to_s+" visitor!"
+    tmp="you are the number "+$visits.to_s+" visitor!"
   elsif hasBoth and !bothFilled
     $problem="you didnt enter full name"
-    tmp="you are the number "+$views.to_s+" visitor!"
+    tmp="you are the number "+$visits.to_s+" visitor!"
   end
   tmp
 end
@@ -19,7 +19,7 @@ end
 $client[:index].update_one({:counter => "visits"}, '$inc' => {:value => 1})
 
 #.find gives back a CollectionView so we have to unwrap it to get the value
-$visits=$client[:index].find(:counter=>"visits").to_a[0][:value].to_i
+$visits=$client[:index].find(:counter=>"visits").first[:value].to_i
 
 
 view "index.html"
