2ebee786462e3afd960e47ce5fa798d70ecb0b33
mjrb <www.w.com123@gmail.com>
2017-10-22 22:52:19 -0400

    factor code out of the massive server thread to improve codestyle, add support for static files like images, fix 404 and 500 occurances

--- server.rb
+++ server.rb
@@ -1,12 +1,7 @@
 require "socket"
 require "yaml"
 
-def loadyaml filename
-  yaml=File.read filename
-  YAML.load yaml
-end
-
-$config=loadyaml "config.yaml"
+load "loadConfig.rb"
 
 if $config[:dbtype]=="silly"
   load "db.rb"
@@ -19,75 +14,86 @@ load "js.rb"
 
 webserver=TCPServer.new($config[:ip], $config[:port])
 
+def getTheGet filename
+  get=Hash.new
+  if filename.include? "?"
+    getBegin=filename.index"?"
+    getEnd=filename.length
+    getstr=filename.slice! getBegin..getEnd
+    Processor::scrub getstr
+    get=Processor::parseGet getstr
+  end
+  get
+end
+
+def getFilename session
+  request=$session.gets
+  puts "request"+request
+  
+  trimmedrequest=request.gsub(/GET\ \//, "").gsub(/\ HTTP.*/, "")
+  filename=trimmedrequest.chomp
+  puts filename
+  
+  $get=getTheGet filename
+  puts "get=> "+$get.to_s
+  
+  
+  if filename==""
+    filename+=$config[:homepage]
+  end
+  puts "requested: "+filename
+  filename
+end
+
+def handle404 session, config
+  puts "file not found sending 404"
+  if $config[:noFileCondition]=="phrase"
+    $session.print $config[:noFile]
+  elsif $config[:noFileCondition]=="page"
+    $session.print File.read $config[:noFile]
+  else
+    raise "invalid config: \":404condition: "+$config[:noFileCondition]+"\""
+  end
+end
 
 
 serverThread=Thread.new do
   while ($session = webserver.accept)
-    puts "connection"
-    $session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
-
-    #get filename
-    request=$session.gets
-    #if filename=="favicon.ico"
-      #$session.close
-    #end
     begin
-      puts "request"+request
-    rescue
-      puts "request"+request
-      puts "no request"
-      $session.close
-    end
-    trimmedrequest=request.gsub(/GET\ \//, "").gsub(/\ HTTP.*/, "")
-    filename=trimmedrequest.chomp
-    puts filename
+      puts "connection"
+      $session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
 
-    #process get forums
-    $get=Hash.new
-    if filename.include? "?"
-      getBegin=filename.index"?"
-      getEnd=filename.length
-      getstr=filename.slice! getBegin..getEnd
-      Processor::scrub getstr
-      $get=Processor::parseGet getstr
-    end
-    puts "get=> "+$get.to_s
-
-
-    if filename=="" or /.\//.match filename
-      filename+=$config[:homepage]
-    end
-    puts "requested: "+filename
-
-    if filename!="favicon.ico"
+      #get filename
+      filename=getFilename $session
       #send file
-      begin
-        scriptFilename=filename.insert 0, $config[:root]+"/"
-        scriptFilename=scriptFilename.dup.chop.chop.chop.chop+"rb"
+      scriptFilename=$config[:root]+"/"+filename+".rb"
+      htmlFilename=$config[:root]+"/"+filename+".html"
+      staticFilename=$config[:static]+"/"+filename
+      puts staticFilename
+      if File.exists? scriptFilename
         puts "running script "+scriptFilename
         load scriptFilename
-      rescue
-        puts "no run script sending static file"
-        begin
-          displayfile=File.open(filename, 'r')
-          content=displayfile.read()
-          $session.print content
-        rescue
-          puts "file not found sending 404"
-          if $config[:noFileCondition]=="phrase"
-            $session.print $config[:noFile]
-          elsif $config[:noFileCondition]=="page"
-            $session.print File.read $config[:noFile]
-          else
-            raise "invalid config: \":404condition: "+$config[:noFileCondition]+"\""
-          end
-        end
+      elsif File.exists? htmlFilename
+        puts "sending html file "+htmlFilename
+        displayfile=File.open(htmlFilename, 'r')
+        content=displayfile.read()
+        $session.print content
+      elsif File.exists? staticFilename
+        puts "sending static resorce "+staticFilename
+        displayfile=File.open(staticFilename, 'r')
+        content=displayfile.read()
+        $session.print content
+      else
+        handle404 $session, $config
       end
+    rescue => e
+      puts e.backtrace
+      $session.print $config[:serverError]
     end
     $session.close
   end
 end
-serverThread.abort_on_exception=false
+serverThread.abort_on_exception=true
 
 def view viewName
   content=File.read $config[:root]+"/"+viewName
