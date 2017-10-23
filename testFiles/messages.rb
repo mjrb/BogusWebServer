646ee63078bccd85334f60c9b8d4ad32775547f8
mjrb <www.w.com123@gmail.com>
2017-10-22 23:38:34 -0400

    fixing example functionality

--- testFiles/messages.rb
+++ testFiles/messages.rb
@@ -1,25 +1,40 @@
 $problem=""
 
+$isPwdGood=false
+
 $hasPwd=$get.has_key? :pwd
 $pwdFilled=$get[:pwd]!=""
 
-if $hasPwd
-  pwd=$client[:users].find_one({:name => "admin"}).first[:password]
-  $goodPwd=pwd==$get[:pwd]
-end
-
 if (!$hasPwd or !$pwdFilled)
-  $problem="please enter pasword"
-elsif 
-  
+  $problem="please enter a pasword(it has to be correct)"
+elsif
+  #reaaaaaaly bad security, dont try this at home
+  #the pasword should be stored hashed, and esnt hased via post
+  #also it should send a cookie so you dont have to log in every time
+  pwd=$client[:users].find({:name => "admin"}).first[:password]
+  $isPwdGood=pwd==$get[:pwd]
+  if !$isPwdGood
+    $problem="wrong password"
+  end
 end
 
-def render message
+def renderMessage message
+  puts message
   result="<hr>"
-  result+="<h3>"+message[:email]+"</h3>"
-  result+="from user: "+message[:email]
+  result+="<h3>"+message["title"]+"</h3>"
+  result+="from user: "+message["email"]+"<br>"
+  result+="<p>"+message["message"]+"</p>"
+  result
 end
+
 def messages
-  if 
+  tmp=""
+  if $isPwdGood
+    messages=$client[:messages].find.to_a
+    messages.each do |message|
+      tmp+=renderMessage message
+    end
+  end
+  tmp
 end
 view "messages.html"
